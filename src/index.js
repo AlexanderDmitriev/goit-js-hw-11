e5b40966fabecb6a32adbb055775bf08b8a74f51
AlexanderDmitriev <alex9110077@gmail.com>
2022-01-18 02:47:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import cardRender from '../src/cardRender.hbs';
-import { getImage } from './js/getImage';
 import { markup } from './js/markup';
 import { res } from './js/getImage';
 
